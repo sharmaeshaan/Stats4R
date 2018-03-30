1739bf859e9eca4b37fe97de2ea664b07bd58112
Eshaan Sharma <sharmaeshaan@outlook.com>
2018-03-30 18:30:23 +0530

    create function to scrape posts from pages

--- analyse-r4r.py
+++ analyse-r4r.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 import sqlite3
 import time
 
+conn_1 = sqlite3.connect('r4r_pages.sqlite')
+cur_1 = conn_1.cursor()
+
 def spider_pages(x):
-    # initialize sqlite db and create table
-    conn_1 = sqlite3.connect('r4r_pages.sqlite')
-    cur_1 = conn_1.cursor()
+    # create first table
     cur_1.execute('CREATE TABLE IF NOT EXISTS pages_list (id INTEGER PRIMARY KEY AUTOINCREMENT, page_url BLOB, page_html BLOB)')
 
     start_url = x
@@ -26,6 +27,7 @@ def spider_pages(x):
         crawl_count = crawl_count -1
         get_start_url = requests.get(start_url, headers = set_header)
         soup = BeautifulSoup(get_start_url.text, 'html.parser')
+        # download whole page html into soup_text
         soup_text = str(soup.prettify())
         span_tag = soup.find('span', class_='next-button')
         a_tag = span_tag.find('a')
@@ -37,4 +39,24 @@ def spider_pages(x):
         start_url = next_page
     conn_1.commit()
 
-spider_pages("https://www.reddit.com/r/r4r")
+def scrape_posts():
+    # fetch downloaded pages from db
+    pages = cur_1.execute('SELECT page_url, page_html FROM pages_list')
+    for i in pages:
+        page_url = i[0]
+        page_html = i[1]
+        soup = BeautifulSoup(page_html, 'html.parser')
+        # structure is div : entry unvoted >>> a : title may-blank
+        r4r_post_divs = soup.find_all('div', class_='entry unvoted')
+        for x in r4r_post_divs:
+            try:
+                r4r_post_atag = x.find('a', class_='title may-blank ')
+                post_url = str('https://www.reddit.com'+atag.attrs['href'])
+                post_title_contents = str(atag.contents[0]).strip()
+                time.sleep(1)
+            except:
+                print('Oops, cannot process <div>. Moving on...')
+
+
+# spider_pages("https://www.reddit.com/r/r4r")
+scrape_posts()
