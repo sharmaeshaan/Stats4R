75e942e23eb8138adf256fb63af09862e4403a8b
Eshaan Sharma <sharmaeshaanw@gmail.com>
2018-04-05 23:54:39 +0530

    output viz. files to /static

--- frontend_r4r.py
+++ frontend_r4r.py
@@ -7,33 +7,33 @@ conn = sqlite3.connect('r4r_posts_breakdown.sqlite')
 cur = conn.cursor()
 
 
-def m2f_total():
+def m2f():
     labels = 'Males', 'Females'
     sizes = [total_males(), total_females()]
-
+    # create new figure and axis
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, labels=labels, startangle=90, autopct='%1.1f%%')
     ax1.axis('equal')
 
-    plt.show()
+    plt.savefig('static/m2f.png', format='png')
 
 def seeking():
     seeking = {'Males\nseeking\nFemales':m4f(), 'Females\nseeking\nMales':f4m(), 'Males\nseeking\nMales':m4m(), 'Females\nseeking\nFemales':f4f()}
     names = list(seeking.keys())
     values = list(seeking.values())
     # define plot size dimensions
-    plt.subplot(121)
-    plt.bar(range(len(seeking)), values, tick_label = names, align='center')
+    fig2, ax2 = plt.subplots()
+    ax2.bar(range(len(seeking)), values, tick_label = names, align='center')
     # plt.xticks(rotation='75')
     # plt.subplots_adjust(bottom=0.3)
-    plt.show()
+    plt.savefig('static/seeking.png', format='png')
 
-def m2f_ages():
+def ages():
     ages = {'Males':mean_age_m(), 'Females':mean_age_f()}
-    names = list(ages.keys())
-    values = list(ages.values())
-    # define plot size dimensions
-    plt.subplot(121)
+    names2 = list(ages.keys())
+    values2 = list(ages.values())
+    # create new figure and axis
+    fig3, ax3 = plt.subplots()
     # Get current axis (gca) of plot
     axes = plt.gca()
     # set 18-40 age range for y-axis
@@ -41,9 +41,11 @@ def m2f_ages():
     # show horizontal grid lines
     axes.yaxis.grid(True)
     # set size of market with 's'
-    plt.scatter(names, values, s=200)
+    ax3.scatter(names2, values2, s=200)
     # set label names
     plt.ylabel('Average Age')
-    plt.show()
+    plt.savefig('static/ages.png', format='png')
 
-m2f_ages()
+m2f()
+seeking()
+ages()
