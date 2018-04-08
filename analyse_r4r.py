3b67d4a7bc8af52de452945ec8d649f8bacdb148
Eshaan Sharma <sharmaeshaanw@gmail.com>
2018-04-08 17:35:17 +0530

    return floating point number upto 2 decimal points instead of integer

--- analyse_r4r.py
+++ analyse_r4r.py
@@ -1,6 +1,7 @@
 import sqlite3
 import time
 import statistics
+from decimal import Decimal
 
 conn = sqlite3.connect('r4r_posts_breakdown.sqlite')
 cur = conn.cursor()
@@ -68,37 +69,46 @@ def f4f():
     f4f = cur.fetchall()
     return len(f4f)
 
+# average number of upvotes on posts by males
 def maleupvotes():
     upvotes = cur.execute('SELECT final_upvotes FROM posts_breakdown WHERE sex = ?', ('m',))
     upvotes_list = list()
     for i in upvotes:
         upvotes_list.append(i[0])
     mean_upvotes = statistics.mean(upvotes_list)
-    return int(mean_upvotes)
+    # round off the average score upto 2 decimal points
+    rounded = round(mean_upvotes, 2)
+    return rounded
 
+# average number of upvotes on posts by females
 def femaleupvotes():
     upvotes = cur.execute('SELECT final_upvotes FROM posts_breakdown WHERE sex = ?', ('f',))
     upvotes_list = list()
     for i in upvotes:
         upvotes_list.append(i[0])
     mean_upvotes = statistics.mean(upvotes_list)
-    return int(mean_upvotes)
+    rounded = round(mean_upvotes, 2)
+    return rounded
 
+# average number of comments on posts by males
 def malecomments():
     comments = cur.execute('SELECT comments_number FROM posts_breakdown WHERE sex = ?', ('m',))
     comments_list = list()
     for i in comments:
         comments_list.append(i[0])
     mean_comments = statistics.mean(comments_list)
-    return mean_comments
+    rounded = round(mean_comments, 2)
+    return rounded
 
+# average number of comments on posts by females
 def femalecomments():
     comments = cur.execute('SELECT comments_number FROM posts_breakdown WHERE sex = ?', ('f',))
     comments_list = list()
     for i in comments:
         comments_list.append(i[0])
     mean_comments = statistics.mean(comments_list)
-    return mean_comments
+    rounded = round(mean_comments, 2)
+    return rounded
 
 # print('Total men: ', total_males())
 # print('Total women: ', total_females())
@@ -109,7 +119,7 @@ def femalecomments():
 # print('Women seeking men: ', f4m())
 # print('Men seeking men: ', m4m())
 # print('Women seeking women: ', f4f())
-# print('Average upvotes score of posts by males: ', maleupvotes())
-# print('Average upvotes score of posts by females: ', femaleupvotes())
+print('Average upvotes score of posts by males: ', maleupvotes())
+print('Average upvotes score of posts by females: ', femaleupvotes())
 print('Average number of comments on posts by males: ', malecomments())
 print('Average number of comments on posts by males: ', femalecomments())
