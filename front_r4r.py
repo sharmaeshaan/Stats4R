7a76bedc8ad7c1a4c3a0327cd1612461269e1832
Eshaan Sharma <sharmaeshaanw@gmail.com>
2018-04-09 22:55:17 +0530

    make changes to flask app settings

--- front_r4r.py
+++ front_r4r.py
@@ -4,11 +4,12 @@ from analyse_r4r import dates
 startdate = dates().get('start date')
 enddate = dates().get('end date')
 
-app = Flask('stats4r')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', startdate = startdate, enddate = enddate)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
